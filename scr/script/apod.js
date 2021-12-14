03ffe72d57ead535f854a40992c3269a930a8e80
Helena Machado <helena.machado.hm@gmail.com>
2021-12-14 17:09:54 -0300

    primeira versao

---
+++ scr/script/apod.js
@@ -0,0 +1,52 @@
+$.ajax({
+  url: `https://api.nasa.gov/planetary/apod?api_key=hjhwrycf6i2tE95zSPWXwJG1kaKJWElWUUvRxjf6`,
+  type: "GET",
+
+  success: function (response) {
+    $("#title").text(response.title);
+
+    if (response.midia_type === "video") {
+      $("#img").hide();
+      $("#video").attr("src", response.hdurl).show();
+    } else {
+      $("#video").hide();
+      $("#img").attr("src", response.hdurl).show();
+    }
+
+    $("#explanation").text(response.explanation);
+
+    if (response.copyright === undefined) {
+      $("#copyright").text(`Copyright: Unknown.`);
+    } else {
+      $("#copyright").text("Copyright: " + response.copyright);
+    }
+  },
+});
+
+$("#button").click(function () {
+  let date = $("#date").val();
+
+  $.ajax({
+    url: `https://api.nasa.gov/planetary/apod?api_key=hjhwrycf6i2tE95zSPWXwJG1kaKJWElWUUvRxjf6&date=${date}`,
+    type: "GET",
+    success: function (response) {
+      $("#title").text(response.title);
+
+      if (response.midia_type === "video") {
+        $("#img").hide();
+        $("#video").attr("src", response.url).show();
+      } else {
+        $("#video").hide();
+        $("#img").attr("src", response.hdurl).show();
+      }
+
+      $("#explanation").text(response.explanation);
+
+      if (response.copyright === undefined) {
+        $("#copyright").text(`Copyright: Unknown.`);
+      } else {
+        $("#copyright").text("Copyright: " + response.copyright);
+      }
+    },
+  });
+});
